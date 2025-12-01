97a4a179ec58294ce64b719610626948b6f831ca
RomeoAtoyan <atoyanr@gmail.com>
2025-12-01 21:43:03 +0100

    darker border on item

--- components/item.tsx
+++ components/item.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Item = () => {
   return (
-    <article className="relative z-0 aspect-[3/4] max-w-[220px] border w-full bg-black flex items-start justify-start pt-4 pl-4">
+    <article className="relative z-0 aspect-[3/4] max-w-[220px] border border-gray-800 w-full bg-black flex items-start justify-start pt-4 pl-4">
       <img
         className="absolute inset-0 w-full h-full object-cover z-[-1] opacity-55"
         src="https://img.freepik.com/premium-photo/armenian-bbq-excellence-khorovats-grilled-delight_1077802-12812.jpg"
